c64f64d0c1799de75eb4a1d82a537bb74a546ba3
Sasika Chandila <sasikachandila@gmail.com>
2024-01-01 09:13:15 +0530

    update_cs

---
+++ Apply.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Student_Registration_Form
+{
+    partial class Apply
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Apply));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_Display = new System.Windows.Forms.Button();
+            this.btn_reg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.panel1.Location = new System.Drawing.Point(412, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(536, 534);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btn_update.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_update.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_update.Location = new System.Drawing.Point(97, 145);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(212, 84);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = false;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btn_delete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_delete.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(97, 264);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(212, 84);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_Display
+            // 
+            this.btn_Display.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btn_Display.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Display.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Display.Location = new System.Drawing.Point(97, 382);
+            this.btn_Display.Name = "btn_Display";
+            this.btn_Display.Size = new System.Drawing.Size(212, 84);
+            this.btn_Display.TabIndex = 4;
+            this.btn_Display.Text = "Display";
+            this.btn_Display.UseVisualStyleBackColor = false;
+            this.btn_Display.Click += new System.EventHandler(this.btn_Display_Click);
+            // 
+            // btn_reg
+            // 
+            this.btn_reg.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btn_reg.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_reg.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reg.Location = new System.Drawing.Point(97, 27);
+            this.btn_reg.Name = "btn_reg";
+            this.btn_reg.Size = new System.Drawing.Size(212, 84);
+            this.btn_reg.TabIndex = 5;
+            this.btn_reg.Text = "Registration";
+            this.btn_reg.UseVisualStyleBackColor = false;
+            this.btn_reg.Click += new System.EventHandler(this.btn_reg_Click);
+            // 
+            // Apply
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.ClientSize = new System.Drawing.Size(944, 530);
+            this.Controls.Add(this.btn_reg);
+            this.Controls.Add(this.btn_Display);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.panel1);
+            this.Name = "Apply";
+            this.Text = "Apply";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_Display;
+        private System.Windows.Forms.Button btn_reg;
+    }
+}
